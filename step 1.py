f8f9003e3a6890df2ec177c464780fa8357d6462
Kristina Puzak <kristina.puzak@tufts.edu>
2019-02-24 18:59:17 -0500

    update

--- step 1.py
+++ step 1.py
@@ -3,27 +3,35 @@
 import math
 import numpy as np
 
-def update_system(acc,pos,vel,time1,time2):
-    dt = time2-time1
-    posNext = #eq for position of a pendulum
-    velNext = #eq for velocity of a pendulum
-    return posNext, velNext
+angle = [0]
+vel = [0]
+acc = [#]
+length = #.25
+angleNext = math.pi / 6
+time = np.linspace(0,10,10)
 
-def print_system(time,pos,vel):
+
+def update_system(acc,angle,vel,time1,time2):
+    dt = time2 - time1
+    angleNext = #eq for angle of a pendulum #angle + vel * dt
+    velNext = #eq for velocity of a pendulum #vel + acc * dt
+    accNext = #(-9.81 * math.sin(angleNext)) / length
+    return angleNext, velNext, accNext
+
+def print_system(time,angle,vel,acc):
     print("TIME: ", time)
-    print("POSITION: ", pos)
-    print("VELOCITY: ", vel, "\n")
+    print("ANGLE: ", angle)
+    print("VELOCITY: ", vel)
+    print("ACCELERATION: ", acc, "\n")
 
-pos = [0]
-vel [0]
-acc = #initial conditions of acceleration for a pendulum
-time = np.linspace#()
-print_system(time[0],pos[0],vel[0])
+
+print_system(time[0],angle[0],vel[0])
 
 i = 1
-while i < len(time):
-    posNext, velNext = update_system(acc[i],pos[i-1],vel[i-1],time[i]) #not sure if this line is right
-    pos.append(posNext)
+while i < len(time)-1:
+    angleNext, velNext, accNext = update_system(acc[i],angle[i-1],vel[i-1],time[i-1],time[i])
+    angle.append(angleNext)
     vel.append(velNext)
-    print_system(time[i],pos[i],vel[i])
+    acc.append(accNext)
+    print_system(time[i],angle[i],vel[i],acc[i])
     i += 1
