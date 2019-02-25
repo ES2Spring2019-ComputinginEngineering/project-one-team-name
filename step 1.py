f2da11f22967f25ecc79e6302cb73253a043fd41
BrettSilv12 <brett.silv12@gmail.com>
2019-02-25 15:57:24 -0500

    Update step 1.py

--- step 1.py
+++ step 1.py
@@ -3,23 +3,22 @@
 import math
 import numpy as np
 
-angle = [0]
 vel = [0]
 acc = [0]
-length = .3
+length = 1
 angleNext = math.pi / 6
-time = np.linspace(0,10,10)
-i = 0
-anglNext = 0
-velNext = 0
-accNext = 0
+angle = [angleNext]
+time = np.linspace(0,10,10000)
+#anglNext = 0
+#velNext = 0
+#accNext = 0
 
 
 def update_system(acc,angle,vel,time1,time2):
     dt = time2 - time1
     angleNext = angle + vel * dt
     velNext = vel + acc * dt
-    accNext = (-9.81 * math.sin(angleNext)) / length
+    accNext = 9.81 * (math.pi/2 - angleNext) / length
     return angleNext, velNext, accNext
 
 def print_system(time,angle,vel,acc):
@@ -29,7 +28,6 @@ def print_system(time,angle,vel,acc):
     print("ACCELERATION: ", acc, "\n")
 
 
-print_system(time[0],angle[0],vel[0],acc[0])
 
 i = 1
 while i < len(time)-1:
@@ -37,5 +35,8 @@ while i < len(time)-1:
     angle.append(angleNext)
     vel.append(velNext)
     acc.append(accNext)
-    print_system(time[i],angle[i],vel[i],acc[i])
+    if i % 100 == 0:
+        print_system(time[i],angle[i],vel[i],acc[i])
     i += 1
+
+print_system(time[0],angle[0],vel[0],acc[0])
