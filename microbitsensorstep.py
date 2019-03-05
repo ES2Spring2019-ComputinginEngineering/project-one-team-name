16bdf74f9d679c4e2827951e78079bc626303905
BrettSilv12 <brett.silv12@gmail.com>
2019-03-04 21:01:28 -0500

    Update microbitsensorstep.py

--- microbitsensorstep.py
+++ microbitsensorstep.py
@@ -1,14 +1,16 @@
-import random
+import random as r
 import microbit as m
-import step1 as stp
 filename = "pendulumdata_" + str(r.randint(1,9999))+".txt"
 
 with open(filename, 'w') as my_file:
     start_time = m.running_time()
     while not m.button_b.is_pressed():
         m.sleep(100)
-        accel_x = accelerometer.get_x()
-        accel_y = accelerometer.get_y()
-        accel_z = accelerometer.get_z()
-        time = microbit.running_time() - start_time
-        my_file.write()
+        accel_x = m.accelerometer.get_x()
+        accel_y = m.accelerometer.get_y()
+        accel_z = m.accelerometer.get_z()
+        time = m.running_time() - start_time
+        datafrom = str(accel_x) + " " + str(accel_y) + " " + str(accel_z) + " " + str(time)
+        my_file.write(datafrom+'\n')
+        m.sleep(50)
+my_file.close
