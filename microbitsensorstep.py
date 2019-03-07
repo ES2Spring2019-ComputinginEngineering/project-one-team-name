ac46c620faee8c4b6e7a716561bec7573474481a
BrettSilv12 <brett.silv12@gmail.com>
2019-03-07 16:14:35 -0500

    Update microbitsensorstep.py

--- microbitsensorstep.py
+++ microbitsensorstep.py
@@ -10,7 +10,7 @@ with open(filename, 'w') as my_file:
         accY = m.accelerometer.get_y()
         accZ = m.accelerometer.get_z()
         time = m.running_time() - start_time
-        datafrom = str(time) + " " + str(accX) + " " + str(accY) + " " + str(accZ)
+        datafrom = str(time) + "," + str(accX) + "," + str(accY) + "," + str(accZ)
         my_file.write(datafrom+' \n') #writes microbit data onto a txt file
         m.sleep(50)
 my_file.close
