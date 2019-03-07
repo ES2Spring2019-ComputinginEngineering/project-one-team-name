a8380ae9b0cb62c54680fac70936e27130330211
BrettSilv12 <brett.silv12@gmail.com>
2019-03-07 00:36:30 -0500

    updates

--- parsingcode.py
+++ parsingcode.py
@@ -1,14 +1,22 @@
 ########################################################################################################
 ###parsing
 ########################################################################################################
-a =input("What is your File Name?")
-if ".txt" in a:
-    filename = a
-else:
-    filename = a + ".txt"
-print(filename) #above ensures proper file is opened for parsing
+#a =input("What is your File Name?")
+#if ".txt" in a:
+    #filename = a
+#else:
+    #filename = a + ".txt"
+#print(filename)
+#above ensures proper file is opened for parsing (keep as comment)
+########INCLUDE ABOVE IF YOU WANT USER INPUT TO DEFINE THE FILE NAME INSTEAD OF MANUAL ENTRY
 
-fin = open(filename)
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+filename = "pendulumdata_4536length1.txt"
+print(filename)
+###change file name on above line to analyze other data files (DELETE ABOVE LINE IF YOU WANT USER INPUT TO DEFINE FILE NAME)
 
 list_lines = []
 time = []
@@ -17,9 +25,47 @@ accY = []
 accZ = []
 angle = []
 #above creates lists for the individual variables
-for line in fin:
-    time.append(int(line.split("\t")[0]))
-    accX.append(int(line.split("\t")[1]))
-    accY.append(int(line.split("\t")[2]))
-    accZ.append(int(line.split("\t")[3]))
-    angle.append(math.atan2(-1*int(line.split("\t")[1]))/ int(line.split("\t")[2]))
+with open (filename, "r") as file:
+
+    for line in file:
+
+        line = line.rstrip().split(" ")
+
+        Yacc = float(line[2])
+        
+        Xacc = float(line[1])
+
+        t = int(line[0])
+
+        accY.append(Yacc)
+
+        time.append(t)
+
+
+
+t = np.asarray(t)
+
+#with open (filename, "r") as file:
+ #   for line in file:
+  #      time.append(int(line.split(" ")[0]))
+   #     accX.append(float(line.split(" ")[1]))
+    #    accY.append(float(line.split(" ")[2]))
+     #   accZ.append(float(line.split(" ")[3]))
+        #i = 0
+        #while i <= len(accX):
+         #   angle.append(math.atan(-1* accX[i] / accY[i] * 100))
+          #  i = i + 1
+#above splits the values in the .txt file into individual variables which are entered into arrays from lines 17-21
+print("times: ",time)
+print("accX: ",accX)
+print("accY: ",accY)
+print("accZ: ",accZ)
+
+#plt.subplot(3,1,1)
+#plt.plot(time[:-1], angle, 'ro--')
+
+#plt.xlabel('Time (seconds)')
+#plt.ylabel('Angle (rad)')
+#plt.title('Angle vs Time')
+#plt.xlim((0, 20))
+#plt.grid()
